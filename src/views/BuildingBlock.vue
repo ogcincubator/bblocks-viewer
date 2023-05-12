b57b5f4093bad3387fd393c6ffaf178cad7efddc
Alejandro Villar <avillar@ticnor.es>
2023-05-12 13:32:36 +0200

    Start working on examples and how to use

--- src/views/BuildingBlock.vue
+++ src/views/BuildingBlock.vue
@@ -24,6 +24,11 @@
             <div class="description" v-html="description.content"></div>
           </v-expansion-panel-text>
         </v-expansion-panel>
+        <v-expansion-panel title="Examples">
+          <v-expansion-panel-text>
+            <em>Coming soon...</em>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
         <v-expansion-panel title="Sources" v-if="bblock.sources && bblock.sources.length">
           <v-expansion-panel-text>
             <v-list>
@@ -40,7 +45,50 @@
           </v-expansion-panel-text>
         </v-expansion-panel>
         <v-expansion-panel title="How to use">
-
+          <v-expansion-panel-text class="how-to">
+            With this building block, I want to...
+            <v-tabs v-model="howToTab" align-tabs="center">
+              <v-tab value="schema">use its schema</v-tab>
+              <v-tab v-if="bblock.ldContext" value="json-ld">use its JSON-LD context</v-tab>
+              <v-tab value="reuse-bb">reuse it in another BB</v-tab>
+            </v-tabs>
+            <v-window v-model="howToTab">
+              <v-window-item value="schema">
+                Schema
+              </v-window-item>
+              <v-window-item value="json-ld">
+                <p>This building block's JSON-LD context can be found at:</p>
+                <v-text-field
+                  readonly
+                >
+                  {{ bblock.ldContext }}
+                  <template v-slot:append-inner>
+                    <v-icon @click.prevent="copyToClipboard(bblock.ldContext)" title="Copy to clipboard">
+                      mdi-clipboard
+                    </v-icon>
+                  </template>
+                </v-text-field>
+                <p>You can include this JSON-LD context in your own JSON objects as follows:</p>
+                <v-textarea
+                  readonly
+                  :model-value="jsonLdExample"
+                  class="monospace"
+                  rows="7"
+                >
+                  <template v-slot:append-inner>
+                    <v-icon @click.prevent="copyToClipboard(jsonLdExample)" title="Copy to clipboard">
+                      mdi-clipboard
+                    </v-icon>
+                  </template>
+                </v-textarea>
+                <p>If you have additional JSON-LD that you would like to use, just add it to the <code>@context</code> array above,
+                either as a URL or as a JSON-LD context object.</p>
+              </v-window-item>
+              <v-window-item value="reuse-bb">
+                REUSE
+              </v-window-item>
+            </v-window>
+          </v-expansion-panel-text>
         </v-expansion-panel>
       </v-expansion-panels>
     </template>
@@ -63,10 +111,25 @@ export default {
         loading: false,
       },
       loading: false,
+      howToTab: 'schema',
     };
   },
   mounted() {
     this.loadBBlock();
+  },
+  computed: {
+    jsonLdExample() {
+      if (!this.bblock?.ldContext) {
+        return '';
+      }
+      return `{
+    "@context": [
+      "${this.bblock.ldContext}"
+    ],
+    "name": "John Smith",
+    "age": 29
+}`
+    }
   },
   methods: {
     loadBBlock() {
@@ -95,6 +158,9 @@ export default {
           this.description.loading = false;
         });
     },
+    copyToClipboard(text) {
+      navigator?.clipboard?.writeText(text);
+    },
   },
   watch: {
     bblock() {
@@ -109,5 +175,11 @@ export default {
   p {
     margin-bottom: 0.6em;
   }
+}
+.how-to .v-tab {
+  text-transform: none !important;
+}
+.monospace {
+  font-family: monospace;
 }
 </style>

7472ea0ac5cda2dbb148b89cd9f201e24546c7b3
Alejandro Villar <avillar@ogc.org>
2025-04-29 11:56:46 +0200

    Show validation banner on Validation tab, too

--- src/components/BuildingBlock.vue
+++ src/components/BuildingBlock.vue
@@ -43,53 +43,7 @@
             <v-window-item value="about" class="tab-content-about pa-1" :transition="false" :reverse-transition="false">
               <v-row>
                 <v-col>
-                  <v-alert v-if="register.validationReport || !bblock.validationPassed" :type="bblock.validationPassed ? 'success' : 'error'">
-                    <span v-if="bblock.validationPassed">
-                      All examples and tests for this building block pass validation.
-                    </span>
-                    <span v-else>
-                      Examples or tests for this building block have validation errors
-                    </span>
-                    <div v-if="register?.validationReport" class="float-end" >
-                      <v-btn
-                        size="small"
-                        :href="`${register.validationReport}#bblock-${bblock.itemIdentifier}`"
-                        prepend-icon="mdi-clipboard-check-outline"
-                        target="_blank"
-                        :class="{ 'with-dropdown': bblock.testOutputs }"
-                      >
-                        Validation report
-                      </v-btn>
-                      <v-menu v-if="bblock.testOutputs" location="bottom end">
-                        <template #activator="{ props }">
-                          <v-btn size="small" v-bind="props" class="dropdown-button">
-                            <v-icon>mdi-menu-down</v-icon>
-                          </v-btn>
-                        </template>
-                        <v-list density="compact" style="padding: 0">
-                          <v-list-item
-                            density="compact"
-                            :href="bblock.testOutputs"
-                            target="_blank"
-                          >
-                            <v-icon>mdi-playlist-check</v-icon>
-                            Test output files
-                          </v-list-item>
-                        </v-list>
-                      </v-menu>
-                    </div>
-                    <div v-else class="float-end">
-                      <v-btn class="float-end"
-                             size="small"
-                             v-if="!register?.validationReport && bblock.testOutputs"
-                             :href="bblock.testOutputs"
-                             prepend-icon="mdi-open-in-new"
-                             target="_blank"
-                      >
-                        View test outputs
-                      </v-btn>
-                    </div>
-                  </v-alert>
+                  <ValidationBanner :register="register" :bblock="bblock"></ValidationBanner>
                 </v-col>
               </v-row>
               <v-row v-if="bblock.description" >
@@ -241,6 +195,7 @@
               :reverse-transition="false"
               v-if="shaclRules?.length"
             >
+              <ValidationBanner :register="register" :bblock="bblock" class="mb-2 mx-1"></ValidationBanner>
               <p class="mb-2">
                 The following sets of SHACL rules are used to validate this building block:
               </p>
@@ -338,11 +293,13 @@ import OntologyViewer from "@/components/bblock/OntologyViewer.vue";
 import SemanticUplift from "@/components/bblock/SemanticUplift.vue";
 import {useNavigationStore} from "@/stores/navigation";
 import TransformsViewer from "@/components/bblock/TransformsViewer.vue";
+import ValidationBanner from "@/components/bblock/ValidationBanner.vue";
 
 const navigationStore = useNavigationStore();
 
 export default {
   components: {
+    ValidationBanner,
     TransformsViewer,
     SemanticUplift,
     OntologyViewer,
---
+++ src/components/bblock/ValidationBanner.vue
@@ -0,0 +1,59 @@
+<script setup>
+import {toRefs} from "vue";
+
+const props = defineProps({
+  register: Object,
+  bblock: Object,
+});
+const {register, bblock} = toRefs(props);
+</script>
+<template>
+  <v-alert v-if="register.validationReport || !bblock.validationPassed"
+           :type="bblock.validationPassed ? 'success' : 'error'">
+    <span v-if="bblock.validationPassed">
+      All examples and tests for this building block pass validation.
+    </span>
+    <span v-else>
+      Examples or tests for this building block have validation errors
+    </span>
+    <div v-if="register?.validationReport" class="float-end">
+      <v-btn
+        size="small"
+        :href="`${register.validationReport}#bblock-${bblock.itemIdentifier}`"
+        prepend-icon="mdi-clipboard-check-outline"
+        target="_blank"
+        :class="{ 'with-dropdown': bblock.testOutputs }"
+      >
+        Validation report
+      </v-btn>
+      <v-menu v-if="bblock.testOutputs" location="bottom end">
+        <template #activator="{ props }">
+          <v-btn size="small" v-bind="props" class="dropdown-button">
+            <v-icon>mdi-menu-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list density="compact" style="padding: 0">
+          <v-list-item
+            density="compact"
+            :href="bblock.testOutputs"
+            target="_blank"
+          >
+            <v-icon>mdi-playlist-check</v-icon>
+            Test output files
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+    <div v-else class="float-end">
+      <v-btn class="float-end"
+             size="small"
+             v-if="!register?.validationReport && bblock.testOutputs"
+             :href="bblock.testOutputs"
+             prepend-icon="mdi-open-in-new"
+             target="_blank"
+      >
+        View test outputs
+      </v-btn>
+    </div>
+  </v-alert>
+</template>
